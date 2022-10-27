7640a14b7b2d786550c0b14e1d6af361dbc899ee
sandeepcnath <sandeepcnath@gmail.com>
2022-10-27 12:44:24 +0400

    banner layout update

--- src/components/homeBanner/index.js
+++ src/components/homeBanner/index.js
@@ -7,34 +7,39 @@ import WhatsAppLogo from "../../images/icons/whatsapp.svg"
 
 const HomeBanner = ({ data }) => {
   const homeData = data.prismicHomepage.data
-  const backgroundUrl = homeData.banner_background_image.fluid.src
   return (
-    <section
-      className="banner"
-      style={{
-        backgroundImage: `url(${backgroundUrl})`,
-      }}
-    >
-      <div className="section__inner">
-        <PrismicRichText field={homeData.main_title.richText} />
-        <PrismicRichText field={homeData.main_subtitle.richText} />
-        <div className="button-wrap">
-          <Link to={homeData.primary_cta.url} className="button button_primary">
-            {homeData.primary_cta_text.text}
-          </Link>
-          <Link
-            to={homeData.secondary_cta.url}
-            className="button button_primary button_secondary"
-          >
-            {homeData.secondary_cta_text.text}
-          </Link>
-          <a
-            href={homeData.whatsapp.url}
-            className="button button_icon"
-            title="click here to chat with us in WhatsApp"
-          >
-            <img src={WhatsAppLogo} />
-          </a>
+    <section className="banner">
+      <div className="section__inner banner__inner">
+        <div className="banner__content-wrap">
+          <PrismicRichText field={homeData.main_title.richText} />
+          <PrismicRichText field={homeData.main_subtitle.richText} />
+          <div className="button-wrap">
+            <Link
+              to={homeData.primary_cta.url}
+              className="button button_primary"
+            >
+              {homeData.primary_cta_text.text}
+            </Link>
+            <Link
+              to={homeData.secondary_cta.url}
+              className="button button_primary button_secondary"
+            >
+              {homeData.secondary_cta_text.text}
+            </Link>
+            <a
+              href={homeData.whatsapp.url}
+              className="button button_icon"
+              title="click here to chat with us in WhatsApp"
+            >
+              <img src={WhatsAppLogo} />
+            </a>
+          </div>
+        </div>
+        <div className="banner__img-wrap">
+          <img
+            src={homeData.banner_background_image.fluid.src}
+            srcSet={homeData.banner_background_image.fluid.srcSet}
+          />
         </div>
       </div>
     </section>
